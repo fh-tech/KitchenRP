51b6ac8d2d7e37fe60a9a51b2419feb610364f45
Daniel Kudernatsch <d.kudernatsch@outlook.com>
2019-11-09 21:16:37 +0100

    Tests and comments for user activation

--- KitchenRP.Web/Controllers/UserController.cs
+++ KitchenRP.Web/Controllers/UserController.cs
@@ -16,14 +16,26 @@ namespace KitchenRP.Web.Controllers
         {
             _userService = service;
         }
-
+        /// <summary>
+        ///     Gets a specific user data by their id.
+        ///     If the requested user corresponds to the current no special role is required,
+        ///     otherwise only admin roles may access others user data
+        /// </summary>
+        /// <param name="id">Id of the requested user</param>
+        /// <returns>User data</returns>
         [HttpGet("{id}")]
         public async Task<IActionResult> GetById(long id)
         {
             var user = await _userService.UserById(id);
-            return Ok(user);
+            return Ok(Mapper.Map(user));
         }
-
+        /// <summary>
+        ///     Activates a user
+        ///     After this action is successful, they may login with their fh credentials and use this service.
+        ///     Needs Admin role 
+        /// </summary>
+        /// <param name="model">fh userid and optional email, if no email is provided the fh email is discovered from the ldap server</param>
+        /// <returns>URI of the created user resource and user ID</returns>
         [HttpPost]
         public async Task<IActionResult> ActivateUser(UserActivationRequest model)
         {
